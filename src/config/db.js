40a01f0649526aca6dab971c95df7698a50035e9
BrayanSena1 <brayan_moralesm@soy.sena.edu.co>
2025-10-29 22:26:05 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,12 @@
-// src/config/db.js
 import mongoose from "mongoose";
+mongoose.set("strictQuery", true);
 
 export async function connectDB(uri = process.env.MONGODB_URI) {
   try {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(uri, { serverSelectionTimeoutMS: 15000 });
-    console.log("MongoDB OK:", mongoose.connection.name);
+    await mongoose.connect(uri, { bufferCommands: false, serverSelectionTimeoutMS: 15000 });
+    // console.log("MongoDB OK:", mongoose.connection.name);
   } catch (e) {
     console.error("MongoDB error:", e.message);
     throw e;
   }
 }
-
